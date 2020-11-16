481983bd23adcaff1004e7ffdd598a3ae94157b2
Donnel <donnel206@hotmail.com>
2020-11-16 10:59:37 +0100

    Add project files.

---
+++ PropertieStudent/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertieStudent
+{
+    public class Student
+    {
+       
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set {
+                if (value >= 18 || value <=30)
+                {
+                    _age = value;
+                }
+                else { Console.WriteLine("Invalid range of age"); }
+            }
+        }
+
+
+        private string _bankCard;
+
+        public string BankCard
+        {
+            get { 
+                string output = "***.***.***" + _bankCard.Substring(_bankCard.Length - 4);
+                return output;
+               // return _bankCard; 
+            }
+            set { _bankCard = value; }
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Age = 35;
+
+            Console.WriteLine("Give your bankCard in");
+            student.BankCard = Console.ReadLine(); 
+            Console.WriteLine($"Your Banck Cad number{student.BankCard}");
+
+        }
+    }
+}
---
+++ PropertiesDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    public class Student
+    {
+        private string _name;
+        //const double PI = 3.14;
+        //private DateTime _birthdate;
+        //private int _age;
+
+        public void SetName(string name)
+        {
+            if (!String.IsNullOrEmpty(name))
+            {
+                this._name = name;
+            }
+            else
+            {
+                Console.WriteLine("Name is Empty");
+                
+            }
+
+        }
+        public void GetName(string name)
+        {
+            if (!String.IsNullOrEmpty(name))
+            {
+                this._name = name;
+            }
+            else
+            {
+                Console.WriteLine("Name is Empty");
+                return _name;
+            }
+            return _name;
+        }
+
+        #region
+        //public void SetBirthdate(DateTime birthdate)
+        //{
+        //    _birthdate = birthdate;
+        //}
+
+        //public DateTime GetBirthDate()
+        //{
+        //    return _birthdate;
+        //}
+
+        //public int GetAge()
+        //{
+        //    TimeSpan span = DateTime.Today - _birthdate;
+        //    var _age = span.Days / 365;
+        //    return _age;
+        //}
+        #endregion
+
+
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+                Student student = new Student();
+                student.SetName(" ");
+
+                Console.WriteLine(student.Getname());
+        }
+    }
+}
